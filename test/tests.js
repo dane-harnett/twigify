c0940313b0553621e723b81f43b31cd38ea66905
Scott Warren <scott_warren@flightcentre.com>
2015-01-17 22:57:39 +1000

    Updates to tests

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,29 @@
-var should = require('should'),
-    twigify = require('../twigify');
+var should  = require('should'),
+    twigify = require('../twigify'),
+    twig    = require('twig').twig;
+
+/**
+ * use this in the tests to render the returned string from Twigify into a Twig
+ * rendered string, with optionally your Object containing the variables that your
+ * Twig template requires
+ * @param  {String} twigifyRender       String that Twigify returns, that you need
+ *                                      to render still
+ * @param  {Object} twigVars            (Optional) Any variables within an Object that your
+ *                                      template requires
+ * @return {String}                     rendered Twig template as a string
+ */
+var render = function(twigifyRender, twigVars) {
+  // Create an empty object if we're passed no twigVars
+  twigVars = twigVars || {};
+
+  // eval the Twigify string and then use Twig to render it
+  return eval(twigifyRender).render(twigVars);
+};
 
 describe('Twigify', function() {
   describe('#twigify()', function() {
     it('should describe what the function does', function() {
+      twigify();
     });
   });
 
@@ -12,14 +32,17 @@ describe('Twigify', function() {
     });
   });
 
-  describe('#twigify().push()', function() {
-    it('should describe what the function does', function() {
+  describe('#twigify()', function() {
+    it ('should be a function', function() {
+      twigify.should.be.an.instanceOf(Function);
     });
-  });
+    it('should render templates minified', function() {
+      var testMe = twigify.compile('a       bc');
+      testMe = render(testMe);
 
-  describe('#twigify().end()', function() {
-    it('should describe what the function does', function() {
+      testMe.should.equal('a bc');
     });
+
   });
 
   describe('#compile()', function() {
