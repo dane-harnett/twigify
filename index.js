94f05e7a02b35339c194fc13e0d5c7650c6d0b69
Dane Harnett <dane_harnett@flightcentre.com>
2015-01-18 22:04:48 +0000

    Remove including requires for each included template, instead user should manually require all the templates so all are preloaded

--- index.js
+++ index.js
@@ -14,39 +14,19 @@ var minifyDefaults = {
 function compile(id, str) {
   var minified = minify(str, minifyDefaults);
 
-  var template = twig({ id: id, data: minified });
+  var template = twig({
+    id: id,
+    data: minified
+  });
 
   var tokens = JSON.stringify(template.tokens)
 
-  var includes = [];
-
-  // create a list of includes
-  for (var i = template.tokens.length - 1; i >= 0; i--) {
-    if (template.tokens[i].type === 'logic') {
-      if (template.tokens[i].token.type === 'Twig.logic.type.include') {
-        // includes at this level must be relative to the current template file path
-        includes.push(template.tokens[i].token.stack[0].value.replace('/templates/', './'));
-      }
-    }
-  }
-
-  return {
-    includes: includes,
-    // the id will be the filename and path relative to the require()ing module
-    source: 'twig({ id: __filename,  data:' + tokens + ', precompiled: true, allowInlineIncludes: true })'
-  };
+  // the id will be the filename and path relative to the require()ing module
+  return 'twig({ id: __filename,  data:' + tokens + ', precompiled: true, allowInlineIncludes: true })';
 }
 
-function process(tpl) {
-  // build a string that require()s all the sub templates
-  var includeString = '';
-  for (var i = tpl.includes.length - 1; i >= 0; i--) {
-    includeString += 'require(\'' + tpl.includes[i] + '\');\n';
-  }
-
+function process(source) {
   return (
-    'var twig = require(\'twig\').twig;\n' +
-    includeString +
     'module.exports = ' + tpl.source + ';\n'
   );
 }
@@ -56,7 +36,7 @@ function twigify(file, opts) {
   if (!opts) opts = {};
 
   var id = file;
-  // might pass a path via CLI to use for relative file paths
+  // @TODO: pass a path via CLI to use for relative file paths
   //opts.path ? file.replace(opts.path, '') : file;
 
   var buffers = [];
